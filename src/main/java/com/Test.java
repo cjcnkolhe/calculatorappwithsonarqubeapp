82ac9bcbf9280bfe3cb0a61461bf05fdf7e63c12
cjcnkolhe <103464730+cjcnkolhe@users.noreply.github.com>
2026-06-27 13:16:51 +0530

    Update Test.java

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,11 +1,20 @@
 package com;
 
 public class Test {
-	public static void main(String[] args) {
-		Calculator c=new Calculator();
-		c.add(10,10);
-		c.add(10, 10);
-		c.sub(10, 5);
-	}
+	  public static void main(String[] args) {
+
+
+
+        Calculator c = new Calculator();
+
+
+
+        System.out.println("Add: " + c.add(10, 5));
+
+        System.out.println("Multiply: " + c.multiply(10, 5));
+
+        System.out.println("Divide: " + c.divide(10, 0)); // ❌ Runtime crash (bug)
+
+    }
 
 }
